5a2c94456d76a56edb6d38c33caad7f4f9ea6a86
shwetadokhe99@gmail.com <dokheshweta9@gmail.com>
2023-04-01 16:28:23 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { data } from "./data";
+import List from "./List";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className="btn btn-block" onClick={() => setPeople([])}>
+          Clear the List
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
